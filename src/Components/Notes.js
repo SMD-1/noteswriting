8763597a09cb03fc0b7261c342cb221be0abfc0c
Danish <skdanish1999@gmail.com>
2021-07-12 23:58:20 +0530

    style card and delete card

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Grid from "@material-ui/core/Grid";
 // import Paper from "@material-ui/core/Paper";
 import { Container } from "@material-ui/core";
-import Card from "../Notecard/Card";
+import Cards from "../Notecard/Card";
 
 function Notes() {
   const [notes, setNotes] = useState([]);
@@ -12,12 +12,21 @@ function Notes() {
       .then((res) => res.json())
       .then((data) => setNotes(data));
   }, []);
+
+  const handleDelete = async (id) => {
+    await fetch("http://localhost:8000/notes/" + id, {
+      method: "DELETE",
+    });
+
+    const newNotes = notes.filter((note) => note.id != id);
+    setNotes(newNotes);
+  };
   return (
     <Container>
-      <Grid container>
+      <Grid container spacing={3}>
         {notes.map((note) => (
           <Grid item key={note.id} xs={12} lg={4} md={6}>
-            <Card note={note} />
+            <Cards note={note} handleDelete={handleDelete} />
           </Grid>
         ))}
       </Grid>
--- src/Notecard/Card.js
+++ src/Notecard/Card.js
@@ -1,7 +1,30 @@
 import React from "react";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import DeleteOutlined from "@material-ui/icons/DeleteOutlined";
 
-function Card({ note }) {
-  return <div>{note.title}</div>;
+export default function Cards({ note, handleDelete }) {
+  return (
+    <div>
+      <Card elevation={1}>
+        <CardHeader
+          action={
+            <IconButton onClick={() => handleDelete(note.id)}>
+              <DeleteOutlined />
+            </IconButton>
+          }
+          title={note.title}
+          subheader={note.category}
+        />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary">
+            {note.details}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
-
-export default Card;
